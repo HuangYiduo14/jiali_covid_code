4edcac0f1c30b11cd3587ab0c1d3a67f02bd1ce1
huangyiduo <yiduo_huang@berkeley.edu>
2021-07-19 00:01:37 -0700

    bug fixed: FP

--- sir_simulation_removing.py
+++ sir_simulation_removing.py
@@ -332,3 +332,33 @@ result_nstar7_round = SIRsimulation(N, table_n_star=df_cpr, exp_name='nstar7_rou
 
 
 """
+def plot_curve(col,is_cum,fig_number,title):
+    plt.figure(fig_number)
+    if is_cum:
+        plt.plot(result_ind_round[col].cumsum(), label='individual, round')
+        plt.plot(result_nstar1_round[col].cumsum(), label='n1, round')
+        #plt.plot(result_nstar7_round[col].cumsum(), label='n7, round')
+        plt.plot(result_ind_period[col].cumsum(), label='individual, period')
+        plt.plot(result_nstar1_period[col].cumsum(), label='n1, period')
+       # plt.plot(result_nstar7_period[col].cumsum(), label='n7, period')
+    else:
+        plt.plot(result_ind_round[col], label='individual, round')
+        plt.plot(result_nstar1_round[col], label='n1, round')
+        #plt.plot(result_nstar7_round[col], label='n7, round')
+        plt.plot(result_ind_period[col], label='individual, period')
+        plt.plot(result_nstar1_period[col], label='n1, period')
+        #plt.plot(result_nstar7_period[col], label='n7, period')
+    plt.legend()
+    plt.title(title)
+    plt.show()
+#result_ind_period[['S','I','R','SQ','Q']].plot()
+#plt.show()
+
+plot_curve('S',False,0,'S')
+plot_curve('I',False,1,'I')
+plot_curve('n_star',False,2,'N')
+plot_curve('TP',True,3,'TP')
+plot_curve('FP',True,4,'FP')
+plot_curve('TN',True,5,'TN')
+plot_curve('FN',True,6,'FN')
+plot_curve('FP',False,7,'FP(t)')
